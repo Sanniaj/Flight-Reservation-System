f617b9d6ee22b80385859c51347d4f058f6d733a
Sanniaj <budderenti@gmail.com>
2025-07-30 10:05:12 -0700

    integrate location search with flight search, added location validation

--- frontend-search-location/flight-search-location.js
+++ frontend-search-location/flight-search-location.js
@@ -1,25 +1,29 @@
-let departureLocation,arrivalLocation
+import {locationSearch} from "./locationSearch.js";
+
+let departureLocation, arrivalLocation;
 
 const flightSearchResults = () => {
     departureLocation = document.getElementById("departure").value;
     arrivalLocation = document.getElementById("arrival").value;
 
+
+    console.log("Searching departure location:");
+    locationSearch(departureLocation);
+
+    console.log("Searching arrival location:");
+    locationSearch(arrivalLocation);
+
+    saveFlightSearchResults();
     window.location.href = "flight-search-results.html";
-
-    return saveFlightSearchResults(departureLocation, arrivalLocation);
 }
-
 
 const saveFlightSearchResults = () => {
     localStorage.setItem("departureLocation", JSON.stringify(departureLocation));
     localStorage.setItem("arrivalLocation", JSON.stringify(arrivalLocation));
 }
 
-
 const displayFlightSearchResults = () => {
     const dep = JSON.parse(localStorage.getItem("departureLocation"));
     const arr = JSON.parse(localStorage.getItem("arrivalLocation"));
     document.getElementById("searchFlights").innerHTML = `Looking at flights from ${dep} to ${arr}`;
 }
-
-
