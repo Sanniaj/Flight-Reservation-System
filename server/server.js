b4f3b1240aca5609f17c92aa188f8fa312387b44
Timothy Do <thinh_do_94@yahoo.com>
2025-08-07 17:57:35 -0700

    Add server folder for API calls.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 3000;
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "../frontend-search-location")));
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
