0fa1dbe7cf1d081ca46ab06e51e137883a6ae035
Timothy Do <thinh_do_94@yahoo.com>
2025-08-12 23:13:35 -0700

    Added repo+lookup: load in repo, search by confirmation+email

---
+++ server/repo/booking_repo.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * booking_repository.js
+ * 
+ * purrpose: read data from booking.json
+ * 
+ *  function: 
+ *      - loadBookings() -> read booking.json and return an array of booking objects
+ *      = lookupBooking(confirmation, email) -> search the array using findBooking()
+ * 
+ * important object: 
+ *      - booking.confirmation
+ *      - booking.customer.email
+ * 
+ *  Note:
+ *      - booking.json location: /server/data/booking.json
+ *      - Used in booking_router.js, booking_lookup.js
+ * 
+*/
+
+import fs from "fs";
+import path from "path";
+import { findBooking } from "../API/booking/booking_lookup.js";
+
+// path to our booking.json
+const projectRoot = process.cwd(); //process.cwd() - current working directory - take you to our project root. ex: C:/Users/thinh/Documents/GitHub/likeag6/
+const bookingFilePath = path.join(projectRoot, "server", "data", "booking.json"); // expect output: C:/Users/thinh/Documents/GitHub/likeag6/server/data/booking.json
+
+// read booking.json and return an array -> loadBookings()
+export function loadBookings() {
+    const rawBookingJson = fs.readFileSync(bookingFilePath, "utf8");
+    if (!rawBookingJson.trim()) return [];
+    return JSON.parse(rawBookingJson);
+}
+
+// look for a booking using confirmation + email -> lookupBooking()
+export function lookupBooking(confirmation, email) {
+    const bookings = loadBookings();
+    return findBooking(bookings, confirmation, email);
+}
